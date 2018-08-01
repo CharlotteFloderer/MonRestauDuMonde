01a21824cbd4796ea8643e1aa31bae2af827148d
CharlotteFloderer <charlotte.floderer@gmail.com>
2018-08-01 12:22:34 +0200

    App Restau Sans erreurs

---
+++ MonRestauDuMonde/utilitaires/constantes.swift
@@ -0,0 +1,9 @@
+//
+//  constantes.swift
+//  MonRestauDuMonde
+//
+//  Created by Charlotte Floderer on 01/08/2018.
+//  Copyright © 2018 Charlotte Floderer. All rights reserved.
+//
+
+import Foundation
---
+++ MonRestauDuMonde/vue/menuCell.swift
@@ -0,0 +1,13 @@
+//
+//  menuCell.swift
+//  MonRestauDuMonde
+//
+//  Created by Charlotte Floderer on 01/08/2018.
+//  Copyright © 2018 Charlotte Floderer. All rights reserved.
+//
+
+import UIKit
+
+class menuCell: UICollectionViewCell {
+    
+}
